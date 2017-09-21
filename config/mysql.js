681f71de8d30a11e54f0abe85b8339c1b5d716b7
smilecc <sxcuican@163.com>
2017-09-21 23:52:03 +0800

    Debug

--- config/mysql.js
+++ config/mysql.js
@@ -9,6 +9,7 @@ let config = {
     dateStrings: true,      // 时间以字符串形式显示，否则会有类似这样的显示：Thu Apr 14 2016 00:00:00 GMT+0800 (中国标准时间) 17:20:12
 };
 
+console.log(config)
 var db = function () {};
 
 db.prototype.pool = mysql.createPool(config);
